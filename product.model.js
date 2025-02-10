18291a113572fadf37a6cd6b14bba254e05bbbc2
Nsuku Mathebula <mathebulansuku@gmail.com>
2025-02-10 22:06:11 +0200

    mongodb setup

---
+++ product.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const ProductShema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+  },
+
+  quantity: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+
+  image: {
+    type: image,
+    required: false,
+  },
+
+  price: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+});
